2a702470e41626acc778b187520f1a1c8762403c
Mohamed Reda <mhmd.reda186@gmail.com>
2026-02-06 00:16:40 +0200

    Fix unused import error in Experience.tsx
    Remove accidentally added compareByDepth import

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { compareByDepth, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useInView } from 'framer-motion';
 import { useRef } from 'react';
 
